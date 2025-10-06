f613d95d1bc688c5ba025819256ed864a0a7d45f
kbryanhsu <bryanhsu2020@gmail.com>
2025-10-06 22:10:05 +0000

    add src, fix environment install issues

--- setup.sh
+++ setup.sh
@@ -6,14 +6,15 @@ VRNA_URL="https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_7_x/ViennaRNA-2
 command -v conda >/dev/null || { echo "Conda not found. Please install conda/mamba."; exit 1; }
 
 # Install tools via conda and pip
+conda env update -f environment.yml
 conda install -y python=3.12.11
-conda install -y -c conda-forge -c bioconda snakemake
+conda install -y -c conda-forge -c bioconda snakemake git
 conda install -y -c bioconda bowtie2 sam2pairwise mafft
 conda install -y -c conda-forge make pkg-config gsl zlib curl
 pip install biopython pandas torch joblib einops scikit-learn gget # now includes gget, but only for the dependencies
 pip install -q mysql-connector-python biopython
 pip install -q --log log git+https://github.com/pachterlab/gget.git@delphy_dev # this build has support for ncbi_virus
-tar -xjvf ./target_seqs/original/HUMAN.tar.bz2
+tar -xjvf ./target_seqs/original/HUMAN.tar.bz2 -C ./target_seqs/original
 
 # Build & install ViennaRNA into ./src
 set -euo pipefail
